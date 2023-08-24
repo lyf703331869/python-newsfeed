729cec55672a8abe1991a54ac5dfd6770aeb6a31
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-08-24 12:05:59 -0400

    Created dashboard && Added frontend pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from app.routes import home
+from app.routes import dashboard
 
 def create_app(test_config=None):
     app = Flask(__name__, static_url_path='/')
@@ -8,4 +10,8 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return "hello world"
+    
+    app.register_blueprint(home)
+    app.register_blueprint(dashboard)
+    
     return app
