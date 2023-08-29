5165d313fe28e5cec0edcefc193b704c6adfa37d
Yafei Liu <yafeiliu@yafeis-air.mynetworksettings.com>
2023-08-29 18:01:35 -0400

    Added error handling:
    '

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
@@ -8,14 +9,17 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 def signup():
     data = request.get_json()
     db = get_db()
-    
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+    try:
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
-    db.add(newUser)
-    db.commit()
-
+        db.add(newUser)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+        db.rollback()
+        return jsonify(message = 'Signup failed'), 500
     return jsonify(id = newUser.id)
